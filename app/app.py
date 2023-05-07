22c74195f0adfaf4e9b57d876bec1a2c01d24853
iassenegal221 <85522183+iassenegal221@users.noreply.github.com>
2023-05-08 00:21:28 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -40,9 +40,14 @@ def load():
     model_bytes = io.BytesIO(model_response.content)
     model = joblib.load(model_bytes)
 ###### Features #######
+    import requests
+    import joblib
+    import io
+
     features_url = "https://github.com/Alhasdata/loan-scoring/raw/main/app/models/training_features.pkl"
     features_response = requests.get(features_url)
-    model_response.raise_for_status()
+    features_response.raise_for_status()
+
     features_bytes = io.BytesIO(features_response.content)
     features = joblib.load(features_bytes)
 
