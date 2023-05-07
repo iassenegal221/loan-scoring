60adffd0b34f9b9ac25701676ec2de36ff3b843c
iassenegal221 <85522183+iassenegal221@users.noreply.github.com>
2023-05-08 00:05:01 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -31,23 +31,16 @@ def load():
     This functions aims to load data and models
     """
     import os
-    model_url = "https://github.com/Alhasdata/loan-scoring/raw/main/app/models/best_model.pkl"
-    features_url = "https://github.com/Alhasdata/loan-scoring/raw/main/app/models/training_features.pkl"
-    dataframe_url = "https://github.com/Alhasdata/loan-scoring/raw/main/app/models/full_data.pkl"
+    import io
 
+    model_url = "https://github.com/Alhasdata/loan-scoring/raw/main/app/models/best_model.pkl"
     response = requests.get(model_url)
     response.raise_for_status()
-    model = joblib.load(response.content)
 
-    response = requests.get(features_url)
-    response.raise_for_status()
-    features = joblib.load(response.content)
+    model_bytes = io.BytesIO(response.content)
+    model = joblib.load(model_bytes)
 
-    response = requests.get(dataframe_url)
-    response.raise_for_status()
-    dataframe = joblib.load(response.content)
-
-    return model, features, dataframe
+    return model
 
 
 loan_scoring_classifier, training_features, raw_data = load()
