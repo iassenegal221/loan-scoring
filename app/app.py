78e94ceab10f37dd1460ab784ab1cfd28ebf6b9b
iassenegal221 <85522183+iassenegal221@users.noreply.github.com>
2023-05-12 20:57:06 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,24 +1,8 @@
 import streamlit as st
-import joblib
 import requests
 import io
-import numpy as np
-
-def load_model():
-    model_url = "https://github.com/Alhasdata/loan-scoring/raw/main/app/models/best_model.pkl"
-    model_response = requests.get(model_url)
-    model_response.raise_for_status()
-    model_bytes = io.BytesIO(model_response.content)
-    model = joblib.load(model_bytes)
-    return model
-
-def load_features():
-    features_url = "https://github.com/iassenegal221/loan-scoring/raw/main/app/data/training_features.pkl"
-    features_response = requests.get(features_url)
-    features_response.raise_for_status()
-    features_bytes = io.BytesIO(features_response.content)
-    features = np.load(features_bytes, allow_pickle=True)
-    return features
+import joblib
+import pandas as pd
 
 def load_dataframe():
     data_url = "https://github.com/iassenegal221/loan-scoring/raw/main/app/data/full_data.pkl"
@@ -29,8 +13,6 @@ def load_dataframe():
     return dataframe
 
 def main():
-    model = load_model()
-    features = load_features()
     dataframe = load_dataframe()
 
     # Streamlit App
